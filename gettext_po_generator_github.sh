5360cce04c7287db5f75224475adf5aa7b202886
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2024-05-13 21:07:38 -0300

    muda pront de tradução, gpt-3.5-turbo-0125

--- gettext_po_generator_github.sh
+++ gettext_po_generator_github.sh
@@ -94,7 +94,7 @@ sed -i '/"POT-Creation-Date:/d;/"PO-Revision-Date:/d' $DIR/locale/*
 #     sudo sed -i '/Ocp-Apim-Subscription-Key/a "Ocp-Apim-Subscription-Region": "brazilsouth",' /usr/local/lib/node_modules/attranslate/dist/services/azure-translator.js
 # fi
 
-sudo sed -i '/temperature:/s/temperature:.*/temperature: 0,/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
+# sudo sed -i '/temperature:/s/temperature:.*/temperature: 0,/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
 # sudo sed -i 's/Translate the following text from ${args.srcLng} into ${args.targetLng}:/please do not interact, only translate this word or phrase from ${args.srcLng} to ${args.targetLng}:\n/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
 sudo sed -i 's/gpt-3.5-turbo-instruct/gpt-3.5-turbo-0125/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
 cat /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
