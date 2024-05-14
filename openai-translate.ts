88da48553d7679638b4beda7d32e367e1c87fbdc
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2024-05-13 21:48:05 -0300

    original from npm

--- openai-translate.ts
+++ openai-translate.ts
@@ -1,29 +1,19 @@
-import { Configuration, OpenAIApi } from "openai";
-import {
-  TResult,
-  TService,
-  TServiceArgs,
-  TString,
-} from "./service-definitions";
-import { logFatal } from "../util/util";
-import { chunk, flatten } from "lodash";
-
-async function translateSingleString(
-  str: string,
-  args: TServiceArgs
-): Promise<string> {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OpenAITranslate = void 0;
+const openai_1 = require("openai");
+const util_1 = require("../util/util");
+const lodash_1 = require("lodash");
+async function translateSingleString(str, args) {
+  var _a;
   const OPENAI_API_KEY = args.serviceConfig;
   if (!OPENAI_API_KEY || !OPENAI_API_KEY.trim().length) {
-    logFatal(
-      'Missing OpenAI API Key: Please get an API key from https://platform.openai.com/account/api-keys and then call attranslate with --serviceConfig="YOUR API KEY"'
-    );
+    (0, util_1.logFatal)('Missing OpenAI API Key: Please get an API key from https://platform.openai.com/account/api-keys and then call attranslate with --serviceConfig="YOUR API KEY"');
   }
-
-  const configuration = new Configuration({
+  const configuration = new openai_1.Configuration({
     apiKey: OPENAI_API_KEY,
   });
-  const openai = new OpenAIApi(configuration);
-
+  const openai = new openai_1.OpenAIApi(configuration);
   const prompt = generatePrompt(str, args);
   /**
    * https://platform.openai.com/docs/api-reference/completions/create
@@ -31,68 +21,58 @@ async function translateSingleString(
    * We generally recommend altering this or top_p but not both.
    */
   try {
-    const chatCompletion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo-0125",
-      messages: [{role: "user", content: prompt}],
+    const completion = await openai.createCompletion({
+      model: "gpt-3.5-turbo-instruct",
+      prompt,
       temperature: 0.2,
       max_tokens: 2048,
     });
-    const text = chatCompletion.data.choices[0].message;
+    const text = completion.data.choices[0].text;
     if (text == undefined) {
-      logFatal("OpenAI returned undefined for prompt " + prompt);
+      (0, util_1.logFatal)("OpenAI returned undefined for prompt " + prompt);
     }
-
     return text;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
+  }
+  catch (e) {
     if (typeof e.message === "string") {
-      logFatal(
-        "OpenAI: " +
-          e.message +
-          ", Status text: " +
-          JSON.stringify(e?.response?.statusText)
-      );
-    } else {
+      (0, util_1.logFatal)("OpenAI: " +
+      e.message +
+      ", Status text: " +
+      JSON.stringify((_a = e === null || e === void 0 ? void 0 : e.response) === null || _a === void 0 ? void 0 : _a.statusText));
+    }
+    else {
       throw e;
     }
   }
 }
-
-function generatePrompt(str: string, args: TServiceArgs) {
+function generatePrompt(str, args) {
   const capitalizedText = str[0].toUpperCase() + str.slice(1).toLowerCase();
-  return (
-    `Translate the following text from ${args.srcLng} into ${args.targetLng}: ` +
-    capitalizedText
-  );
+  return (`Translate the following text from ${args.srcLng} into ${args.targetLng}: ` +
+  capitalizedText);
 }
-
-async function translateBatch(
-  batch: TString[],
-  args: TServiceArgs
-): Promise<TResult[]> {
-  console.log(
-    "Translate a batch of " + batch.length + " strings with OpenAI..."
-  );
-  const promises: Promise<TResult>[] = batch.map(async (tString: TString) => {
+async function translateBatch(batch, args) {
+  console.log("Translate a batch of " + batch.length + " strings with OpenAI...");
+  const promises = batch.map(async (tString) => {
     const rawResult = await translateSingleString(tString.value, args);
-    const result: TResult = {
+    const result = {
       key: tString.key,
       translated: rawResult.trim(),
     };
     return result;
   });
-  const resolvedPromises: TResult[] = await Promise.all(promises);
+  const resolvedPromises = await Promise.all(promises);
   return resolvedPromises;
 }
-
-export class OpenAITranslate implements TService {
-  async translateStrings(args: TServiceArgs) {
-    const batches: TString[][] = chunk(args.strings, 10);
-    const results: TResult[][] = [];
+class OpenAITranslate {
+  async translateStrings(args) {
+    const batches = (0, lodash_1.chunk)(args.strings, 10);
+    const results = [];
     for (const batch of batches) {
       const result = await translateBatch(batch, args);
       results.push(result);
     }
-    return flatten(results);
+    return (0, lodash_1.flatten)(results);
   }
 }
+exports.OpenAITranslate = OpenAITranslate;
