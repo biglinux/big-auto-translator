f1ca879deb605e4a5b9fb1124821fe800994586c
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2024-05-13 21:04:27 -0300

    muda pront de tradução, gpt-3.5-turbo-0125

--- gettext_po_generator_github.sh
+++ gettext_po_generator_github.sh
@@ -95,9 +95,9 @@ sed -i '/"POT-Creation-Date:/d;/"PO-Revision-Date:/d' $DIR/locale/*
 # fi
 
 sudo sed -i '/temperature:/s/temperature:.*/temperature: 0,/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
-sudo sed -i 's/Translate the following text from ${args.srcLng} into ${args.targetLng}:/please do not interact, only translate this word or phrase from ${args.srcLng} to ${args.targetLng}:/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
+sudo sed -i 's/Translate the following text from ${args.srcLng} into ${args.targetLng}:/please do not interact, only translate this word or phrase from ${args.srcLng} to ${args.targetLng}:\n/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
 sudo sed -i 's/gpt-3.5-turbo-instruct/gpt-3.5-turbo-0125/' /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
-# cat /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
+cat /usr/local/lib/node_modules/attranslate/dist/services/openai-translate.js
 
 for i in $LANGUAGES; do
     if [ "$i" != "$OriginalLang" ]; then
